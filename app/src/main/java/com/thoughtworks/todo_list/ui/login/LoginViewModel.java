90eb109ee9412bf1383656b818562e4d796c9c8d
yangchen <chen.yang1@thoughtworks.com>
2020-07-30 18:25:41 +0800

    verify user info

--- app/src/main/java/com/thoughtworks/todo_list/ui/login/LoginViewModel.java
+++ app/src/main/java/com/thoughtworks/todo_list/ui/login/LoginViewModel.java
@@ -81,12 +81,12 @@ public class LoginViewModel extends ViewModel {
         if (username.contains("@")) {
             return Patterns.EMAIL_ADDRESS.matcher(username).matches();
         } else {
-            return !username.trim().isEmpty();
+            return username.trim().length() >= 3 && username.trim().length() <= 12;
         }
     }
 
     private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 2;
+        return password != null && password.trim().length() >= 6 && password.trim().length() <= 18;
     }
 
     public void insertInitUser() {
