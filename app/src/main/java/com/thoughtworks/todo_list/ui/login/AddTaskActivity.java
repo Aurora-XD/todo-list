d8cca2d6d4ea3301559cb923a2a6fa48ad2711dd
yangchen <chen.yang1@thoughtworks.com>
2020-07-30 22:25:04 +0800

    add calender to add task activity

--- app/src/main/java/com/thoughtworks/todo_list/ui/login/AddTaskActivity.java
+++ app/src/main/java/com/thoughtworks/todo_list/ui/login/AddTaskActivity.java
@@ -2,15 +2,54 @@ package com.thoughtworks.todo_list.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import com.thoughtworks.todo_list.R;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AddTaskActivity extends AppCompatActivity {
+
+    @BindView(R.id.add_task_is_finish)
+    CheckBox mIsFinish;
+
+    @BindView(R.id.add_task_date)
+    Button mBtnDate;
+
+    @BindView(R.id.add_task_remind)
+    Switch mIsRemind;
+
+    @BindView(R.id.add_task_header)
+    EditText mEditHeader;
+
+    @BindView(R.id.add_task_description)
+    EditText mEditDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.add_task_date)
+    void setTaskDeadline(){
+        Calendar now = Calendar.getInstance();
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                mBtnDate.setText(year+"年"+month+"月"+day+"日");
+            }
+        },now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
