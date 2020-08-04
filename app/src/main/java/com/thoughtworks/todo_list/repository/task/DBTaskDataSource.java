90681c35957b127590b24c9bc2e17ea90ad62ce8
yangchen <chen.yang1@thoughtworks.com>
2020-08-04 20:02:06 +0800

    modify task list order in home

--- app/src/main/java/com/thoughtworks/todo_list/repository/task/DBTaskDataSource.java
+++ app/src/main/java/com/thoughtworks/todo_list/repository/task/DBTaskDataSource.java
@@ -16,6 +16,6 @@ public interface DBTaskDataSource extends TaskDateSource {
     @Insert
     Completable saveTask(Task task);
 
-    @Query("select * from task where username = :name order by deadline desc")
+    @Query("select * from task where username = :name order by isFinish asc, deadline desc")
     Maybe<List<Task>> getAllTask(String name);
 }
