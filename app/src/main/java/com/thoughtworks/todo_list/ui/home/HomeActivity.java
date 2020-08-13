f7bf39a1497e3dddf87f00dea3b9533c02c1fac1
yangchen <chen.yang1@thoughtworks.com>
2020-08-13 18:54:35 +0800

    add menu

--- app/src/main/java/com/thoughtworks/todo_list/ui/home/HomeActivity.java
+++ app/src/main/java/com/thoughtworks/todo_list/ui/home/HomeActivity.java
@@ -3,9 +3,13 @@ package com.thoughtworks.todo_list.ui.home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,6 +20,7 @@ import com.thoughtworks.todo_list.R;
 import com.thoughtworks.todo_list.repository.task.TaskRepository;
 import com.thoughtworks.todo_list.repository.task.entity.Task;
 import com.thoughtworks.todo_list.repository.utils.TaskAdapter;
+import com.thoughtworks.todo_list.ui.login.LoginActivity;
 import com.thoughtworks.todo_list.ui.task.AddTaskActivity;
 
 import java.util.Date;
@@ -28,6 +33,9 @@ import butterknife.OnClick;
 public class HomeActivity extends AppCompatActivity {
 
     public static final String TASK_ID = "taskId";
+
+    @BindView(R.id.home_menu_bar)
+    Toolbar mToolbar;
 
     @BindView(R.id.home_recycler_view)
     RecyclerView mRecyclerView;
@@ -51,6 +59,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activiey_home);
 
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
 
         homeViewModel = obtainViewModel();
 
@@ -85,6 +95,22 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        mToolbar.inflateMenu(R.menu.home_menu);//设置右上角的填充菜单
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(R.id.home_menu == item.getItemId()){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void refreshTaskList(){
